c0fffa097b5699299589da9d8ead3a5ed6b3fcd1
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-02-14 10:59:32 +0530

    reviewscontroller update detete funtion 👍

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -48,3 +48,92 @@ export function getReviews(req,res){
         })
      }
 }
+
+//** create a function for delete review  */
+export function deleteReviews(req,res){
+
+    const email = req.params.email;
+   //** admin can delete all reviews,but
+   // customer can delete his's reviews */
+    if(req.user == null){
+        res.status(401).json({
+            message:" Please login and try again"
+        });
+        return 
+    }
+
+    if(req.user.role == "admin")
+    {
+        Review.deleteOne({email : email}).then(()=>{
+            res.json({
+                message : "Review deleted successfully"
+            });
+        }).catch(()=>{
+            res.status(500).json({
+                message : " Review deletion failed"
+            });
+        });
+        return 
+    }
+
+    if(req.user.role == "customer")
+    {
+       if(req.user.email == email)
+       {
+        Review.deleteOne({email : email}).then(()=>{
+            res.json({
+                message : "Review deleted successfully"
+            });
+        }).catch(()=>{
+            res.status(500).json({
+                message : " Review deletion failed"
+            });
+        });
+       }
+       else{
+        res.status(403).json({
+            message : "You are not authorized to perform this action"
+        });
+       }
+       return 
+    }
+
+
+   
+}
+
+export function approvedReview(req,res){
+
+    const email = req.params.email;
+    //** check the user is allready login  */
+    if(req.user == null)
+    {
+        res.status(401).json({
+            message : "Please login and try again"
+        });
+        return 
+    }
+    if(req.user.role == " admin")
+    {
+        Review.updateOne(
+            {
+                email : email 
+            },{
+               isApproved : true
+            }
+        ).then(()=>{
+           res.json({
+            message : " Review approved successfully"
+           }).catch(()=>{
+            res.status(500).json({
+                error : "Review approval failed"
+            });
+           });
+        })
+    }
+    else{
+        res.status(403).json({
+            message: " You are not and admin.Only admins can approve the reviews "
+        });
+    }
+}
--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { addReview } from "../controller/reviewController.js";
+import { addReview, approvedReview, deleteReviews, getReviews } from "../controller/reviewController.js";
 
 
 const reviewRouter = express.Router();
 reviewRouter.post("/",addReview)
-
+reviewRouter.get("/",getReviews)
+reviewRouter.delete("/:email",deleteReviews)
+reviewRouter.put("/approve/:email",approvedReview)
 
 
 export default reviewRouter;
