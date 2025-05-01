9f2de261ea22d75158464f570cd4444c7ab9e393
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-05-01 08:30:29 +0530

    oin order router page get function convert to post function

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -4,7 +4,7 @@ import { createOrder, getQuotetion } from "../controller/ordereController.js";
 const orderRouter = express.Router();
 
 orderRouter.post("/",createOrder);
-orderRouter.get("/quotetion",getQuotetion)
+orderRouter.post("/quotetion",getQuotetion)
 
 
 export default orderRouter;
