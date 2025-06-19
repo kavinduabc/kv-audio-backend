5588cf4fed293048649659b8eda40e4ef7fe1399
kavinduabc <piumalranepura2002@gmail.com>
2025-06-19 08:09:13 +0530

    addfunction image controller succesfully with  it router

--- controller/addImageController.js
+++ controller/addImageController.js
@@ -1,33 +1,33 @@
+ import image from '../models/addImage.js';
 
-
-export async function addImageGallery(req,res){
-     try{
-        if(isItAdmin(req)){
-            const {image,functionName,discription} = req.body;
-
-            if(!image || !functionName || !discription){
-                return res.status(400).json({
-                    message :"Plese file the all fields"
-                })
-            }
-             const newImage = new image
-             (
-                {
-                    image,
-                    functionName,
-                    discription
-                }
-             ).save();
-             res.json({
-                message : "Image added to gallery successfully",
-                newImage
-             })
-        }
-
-     }
-     catch(e){
-        res.status(500).json({
-            message :"Failed to add image to gallery"
+export async function addFunctionImage(res,res){
+    if(!req.user){
+        return res.status(401).json({
+            message:"Pleaes Login and try again"
         })
-     }
+    }
+
+    if(req.user.role !== "admin"){
+        return res.status(403).json({
+            message :"you are not authorized to perform this action"
+        })
+    }
+
+    try{
+        const data =req.body;
+        console.log("adding image data:",data);
+
+        const newImage = new  image(data);
+        await newImage.save();
+
+        res.json({
+            message:"Image added   successfully"
+        })
+    }
+    catch(error){
+        console.error("Image save error:",error.message);
+        res.status(500).json({
+            error:"Image added failed"
+        })
+    }
 }
---
+++ routes/addImageRouter.js
@@ -0,0 +1,8 @@
+import express from "express"
+import {addFunctionImage} from "..//controller/addImageController/js"
+
+const addImageRouter = express.Router()
+
+addImageRouter.post("/", addFunctionImage);
+
+export default addImageRouter;
